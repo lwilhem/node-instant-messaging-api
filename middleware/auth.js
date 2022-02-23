62e49b6769f954a620f6fc81e05b110bffb079d0
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-02-23 14:53:14 +0100

    :construction: auth architecture

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,11 @@
-const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+
+app.post('/login/password',
+  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+  function(req, res) {
+    res.redirect('/~' + req.user.username);
+  });
+
+module.export = app
+
---
+++ model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, index: true, unique: true, required: true, uniqueCaseInsensitive:true },
+    password: { type: String, required: true }
+})
+
+userSchema.plugin(uniqueValidator, { type: 'mongoose-unique-validator' });
