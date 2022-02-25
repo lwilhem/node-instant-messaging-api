d8f1441633ef7416a40630d95f62140e1023043d
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-02-25 19:04:57 +0100

    douleur

--- app.js
+++ app.js
@@ -8,19 +8,28 @@ const app = express()
 
 const mongoose = require('mongoose')
 
+
+
 mongoose.connect('mongodb+srv://user1:AYjrYjrKxHIimC85@cluster0.kvygz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-app.use(cors())
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Access-Control-Allow-Headers','Origin,X-Requested-With,Content,Accept,Content-Type,Authorization'],
+    
+}
 
-app.use((req,res,next)=> {
+app.use(cors(),)
+
+/* app.use((req,res,next)=> {
     res.setHeader('Access-Control-Allow-Origin','*')
     res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content,Accept,Content-Type,Authorization')
-    //res.setHeader('Access-Control-Allow-Origin','GET,POST,PUT,DELETE')
+    res.setHeader('Access-Control-Allow-Origin','GET,POST,PUT,DELETE')
     next()
-})
+}) */
 
 app.use(express.json())
 app.use(express.urlencoded({
